c2a0e982db5004624d890ace21214c11b900bf35
jizhongchao0729 <2538600209@qq.com>
2023-07-04 23:18:31 +0800

    add list

--- DataStructures/List/list..go
+++ DataStructures/List/list..go
@@ -1,33 +1,96 @@
 package main
 
+import "fmt"
+
 type Node struct {
 	Val  int
 	Next *Node
 }
-
-// type Node1 struct {
-// 	val  int
-// 	next *Node
-// }
-// 大写不报错，小写就会报错
-
-func (list Node) PushBack(val int) bool {
-	return true
-}
-func (list Node) PushFont(val int) bool {
-	return true
+type LinkList struct {
+	Head *Node
+	Len  int
 }
 
-func (list Node) Insert(val int, index int) bool {
-	return true
+func (L *LinkList) Init() {
+	L.Head = &Node{}
+	L.Len = 0
+}
+func (L *LinkList) PushBack(val int) {
+	head := L.Head.Next
+	if head == nil {
+		L.Head.Next = &Node{Val: val}
+	} else {
+		for head.Next != nil {
+			head = head.Next
+		}
+		head.Next = &Node{Val: val}
+	}
+	L.Len++
 }
 
-func (list Node) Delete(index int) {
+func (L *LinkList) PushFont(val int) {
+	head := L.Head
+	next := head.Next
+	head.Next = &Node{Val: val}
+	head.Next.Next = next
+	L.Len++
+}
+
+func (L *LinkList) Insert(val int, index int) {
+	head := L.Head
+	i := 0
+	for ; head.Next != nil && i != index; i += 1 {
+		head = head.Next
+	}
+	if head.Next != nil && i == index {
+		next := head.Next
+		head.Next = &Node{Val: val}
+		head.Next.Next = next
+	} else {
+		head.Next = &Node{Val: val}
+	}
+	L.Len++
 
 }
-func (list Node) Update(val int) {
 
+func (L *LinkList) Delete(index int) {
+	if index > L.Len {
+		return
+	}
+	for i, head := 0, L.Head; head.Next != nil; head, i = head.Next, i+1 {
+		if i == index-1 {
+			next := head.Next.Next
+			head.Next = next
+			break
+		}
+	}
+	L.Len--
 }
-func (list Node) Find(val int) {
-
+func (L LinkList) Update(index int, val int) {
+	if index > L.Len {
+		return
+	}
+	for i, head := 0, L.Head; head.Next != nil; head, i = head.Next, i+1 {
+		if i == index {
+			head.Val = val
+			break
+		}
+	}
+}
+func (L LinkList) Find(val int) bool {
+	if L.Len == 0 {
+		return false
+	}
+	for head := L.Head.Next; head != nil; head = head.Next {
+		if head.Val == val {
+			return true
+		}
+	}
+	return false
+}
+func (L LinkList) Print() {
+	for head := L.Head.Next; head != nil; head = head.Next {
+		fmt.Printf("%v\t", head.Val)
+	}
+	fmt.Println("长度为", L.Len)
 }
--- DataStructures/List/main.go
+++ DataStructures/List/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // main 包默认不会加载其他文件， 而其他包都是默认加载的
 // main包下多个文件，需go build/run . 进行编译运行
 // func main() {
@@ -9,5 +7,6 @@ import "fmt"
 // }
 
 func main() {
-	fmt.Printf("地址为%p", &[3]int{1, 2, 3})
+	// fmt.Printf("地址为%p", &[3]int{1, 2, 3})
+	ListCase()
 }
